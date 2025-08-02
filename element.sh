6ead6a58ad3f81cb40b41fefec4c7198448ab945
iliaAmin <ilia9tech@outlook.com>
2025-08-02 06:02:59 -0400

    feat: Query for elemnts created

--- element.sh
+++ element.sh
@@ -7,3 +7,15 @@ if [[ -z $1 ]]; then
   exit
 fi
 
+# Query for element by atomic_number, symbol, or name
+
+ELEMENT_INFO=$($PSQL "
+  SELECT atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type
+  FROM elements
+  JOIN properties USING(atomic_number)
+  JOIN types USING(type_id)
+  WHERE atomic_number::TEXT = '$1'
+     OR symbol = INITCAP('$1')
+     OR name = INITCAP('$1');
+")
+
