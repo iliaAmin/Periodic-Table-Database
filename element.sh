4efdc61acaca8758bb2840b2ee49843fb93b730e
iliaAmin <ilia9tech@outlook.com>
2025-08-02 06:02:15 -0400

    feat: argument validator made

--- element.sh
+++ element.sh
@@ -1,1 +1,9 @@
+!#bin/bash
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+# Check if argument is provided
+if [[ -z $1 ]]; then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
